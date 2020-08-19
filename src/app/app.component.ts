9d412cbb3d13d126ec777b803c04704d7c65728a
kilewa <kilewageorge230@gmail.com>
2020-08-19 14:09:31 +0300

    created detail component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Diary } from './diary';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,5 @@ import { Diary } from './diary';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  diaries:Diary[] = [
-    {id:1, name:'Visited lake Nakuru with college friends'},
-    {id:2,name:'Bought my first car'},
-    {id:3,name:'Great times at Diani Beach'},
-    {id:4,name:'Visited my long time friend Evans'},
-    {id:5,name:'Graduated from Moringa school'},
-    {id:6,name:'My wedding day! What did i say?? haha! Not serious though.'},
-  ];
+  
 }
--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Diary } from '../diary';
 
 @Component({
   selector: 'app-diary',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./diary.component.css']
 })
 export class DiaryComponent implements OnInit {
+  diaries:Diary[] = [
+    {id:1, name:'Visited lake Nakuru .',description:'Had sweet times at Lake Nakuru with college friends'},
+    {id:2,name:'Bought my first car.', description:'This is the day I picked my brand new car from Mombasa port'},
+    {id:3,name:'Great times at Diani Beach.',description:'Visited Diani Beach with my girlfriend.What a time!!'},
+    {id:4,name:'Visited my long time friend Evans.',description:'I met my long time friend after many years since high school'},
+    {id:5,name:'Graduated from Moringa school.',description:'On this day, I graduated from Moringa school. Got certified as a professional software Developer'},
+    {id:6,name:'My wedding day! What did i say?? haha! Not serious though.😄😄😄.',description:'I cant forget this day when i said i do. Am dreaming 😄😄😄😄😄'},
+  ];
 
   constructor() { }
 
